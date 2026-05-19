a2e59e177b2b07e5ab0d6120f07a8975cb2ae541
lowjincpu <Lowjin@protonmail.com>
2026-05-19 17:45:42 +0100

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use getopts::{Matches, Options};
 use regex::{Regex, RegexBuilder};
 use rust_decimal::{Decimal, MathematicalOps};
-//use secp256k1::generate_keypair;
-use secp256k1::{Secp256k1, generate_keypair} ;
+use secp256k1::generate_keypair;
+use secp256k1::Secp256k1;
 use secp256k1::rand::rng;
 use rand::thread_rng;
 use std::process::exit;
